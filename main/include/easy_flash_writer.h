3f8f1bd4766bc1716913a35b0676fdd05391b4d9
Tim <tmtnckff@icloud.com>
2019-08-18 15:27:51 +0200

    added two more variables to store on flash, code cleanup

--- main/include/easy_flash_writer.h
+++ main/include/easy_flash_writer.h
@@ -1,5 +1,5 @@
 /*
- * persistence.h
+ * easy_flash_writer.c
  *
  *  Created on: 23 May 2019
  *      Author: Tim Tenckhoff
@@ -8,22 +8,20 @@
 #ifndef MAIN_INCLUDE_EASY_FLASH_WRITER_H
 #define MAIN_INCLUDE_EASY_FLASH_WRITER_H
 
-#define CONFIG_SECTOR 0x80-4
-#define CONFIG_ADDRESS = (CONFIG_SECTOR * 4096)
+#define _FS_END 0x405FB000
+#define _FS_RANGE 0x40200000
 
 // Enum for the two different types of Data to flash
 typedef enum FlashDataType
 {
-    FLASHDAYS,
-    FLASHHOURS,
+	PREV_STATE,
+	PREV_TIME,
+	TIME_DAY,
+	TIME_NIGHT
 } FlashDataType;
 
 void initFlashWriter();
 void flash_write(void* value, FlashDataType dataType);
 uint8_t flash_read(void* value, FlashDataType dataType);
-
-
-
-
 
 #endif /* MAIN_INCLUDE_EASY_PERSISTENCE_H_ */
